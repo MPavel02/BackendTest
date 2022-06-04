e1596d8db133ad900e9e10dd31fe4983eb30dfdd
Pavel <90553006+MPavel02@users.noreply.github.com>
2022-06-04 19:03:38 +0500

    Create SumOfMass.cs

---
+++ SumOfMass.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace SumOfTwoSmallest
+{
+    public class SumOfMass
+    {
+        public double SumOfSmallest(double[] numbs, int countofnumbers)
+        {
+            double sum = 0;
+
+            if (numbs == null)
+            {
+                throw new ArgumentNullException("Null mass");
+            }
+            if (numbs.Length == 0)
+            {
+                throw new ArgumentNullException("0 elements in mass");
+            }
+            if (countofnumbers > numbs.Length)
+            {
+                throw new IndexOutOfRangeException("Bigger than count of elems in mass");
+            }
+            else
+            {
+                Array.Sort(numbs);
+
+                for(int i = 0; i < numbs.Length; i++)
+                {
+                    sum = sum + numbs[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
